e8bac8c97d7b2bd21f63bdd065476ce11a4e72e4
colinknox <colinwknox@gmail.com>
2026-06-21 15:36:58 -0400

    removed pseudocode and other unnecessary comments.

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,12 +1,5 @@
 import random
 
-# Loop
-    # Ask user for minimum value
-    # Ask user for maximum value
-    # If minimum value > max value:
-        # Print "Min value can't be higher than max value"
-    # break
-
 while True:
     minimum_value = int(input("Choose a minimum value: "))
     maximum_value = int(input("Choose a maximum value: "))
@@ -16,9 +9,6 @@ while True:
         break
 
 number_to_guess = random.randint(minimum_value, maximum_value)
-# print(f"NUMBER TO GUESS = {number_to_guess}")
-
-# number_to_guess = random.randint(1, 100)
 guess_attempts = 0
 
 while True:
