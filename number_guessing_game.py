d8f7a12c4b64ff9bb2fcb1e8d92b35c2d59c97da
colinknox <colinwknox@gmail.com>
2026-06-21 11:22:34 -0400

    user now told congratulations when they guess the correct number. program lets them know how many attempts they made.

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -4,7 +4,6 @@ number_to_guess = random.randint(1, 100)
 guess_attempts = 0
 
 while True:
-    print(f"DEBUG: Number to guess = {number_to_guess}")
     user_guess = int(input("Guess the number (between 1 and 100): "))
     if user_guess > number_to_guess:
         print("Too high!")
@@ -13,9 +12,12 @@ while True:
         print("Too low!")
         guess_attempts += 1
     else:
-        pass
-
-    print(f"Guess attempts = {guess_attempts}")
+        guess_attempts += 1
+        if guess_attempts == 1:
+            print(f"Congratulations! You guessed the number in {guess_attempts} attempt.")
+        else:
+            print(f"Congratulations! You guessed the number in {guess_attempts} attempts.")
+        break
         
 
 
