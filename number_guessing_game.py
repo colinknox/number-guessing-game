54604eb4c5ea796994c4f34dc55477711a2d9f57
colinknox <colinwknox@gmail.com>
2026-06-21 15:36:02 -0400

    wrote pseudocode to allow user to choose min and max values to guess between. i then implemented them.

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,10 +1,28 @@
 import random
 
-number_to_guess = random.randint(1, 100)
+# Loop
+    # Ask user for minimum value
+    # Ask user for maximum value
+    # If minimum value > max value:
+        # Print "Min value can't be higher than max value"
+    # break
+
+while True:
+    minimum_value = int(input("Choose a minimum value: "))
+    maximum_value = int(input("Choose a maximum value: "))
+    if minimum_value > maximum_value:
+        print("Minimum value can't be higher than the maximum value.")
+    else:
+        break
+
+number_to_guess = random.randint(minimum_value, maximum_value)
+# print(f"NUMBER TO GUESS = {number_to_guess}")
+
+# number_to_guess = random.randint(1, 100)
 guess_attempts = 0
 
 while True:
-    user_guess = int(input("Guess the number (between 1 and 100): "))
+    user_guess = int(input(f"Guess the number (between {minimum_value} and {maximum_value}): "))
     if user_guess > number_to_guess:
         guess_attempts += 1
         print("Too high!")
