f42739f1c3669b592710b57c036d8681a84908fe
colinknox <colinwknox@gmail.com>
2026-06-21 10:50:57 -0400

    wrote pseudocode for the game

---
+++ number_guessing_game.py
@@ -0,0 +1,15 @@
+# Generate a random single number between 1 and 100
+# 
+# guess_counter = 0
+# 
+# Loop:
+    # Prompt user to guess the number between 1 and 100
+    # If the guess is too high:
+    #   Print "Too high"
+    #   guess_count += 1
+    # If the guess is too low:
+    #   Print "Too low"
+    #   guess_count += 1
+    # Else:
+    #   Print "Congratulations! You guessed the number in {guess_count} attempts."
+    #   break
