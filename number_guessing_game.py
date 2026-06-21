77a43c51b4fe0f18c4846f6d71f09c36e9f7eb66
colinknox <colinwknox@gmail.com>
2026-06-21 11:13:24 -0400

    game prints 'too high' or 'too low' when user guesses too high or too low. also added attempt counter.

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,9 +1,27 @@
 import random
 
 number_to_guess = random.randint(1, 100)
+guess_attempts = 0
 
 while True:
-    user_guess = int(input("Guess the number (betweeen 1 and 100): "))
+    print(f"DEBUG: Number to guess = {number_to_guess}")
+    user_guess = int(input("Guess the number (between 1 and 100): "))
+    if user_guess > number_to_guess:
+        print("Too high!")
+        guess_attempts += 1
+    elif user_guess < number_to_guess:
+        print("Too low!")
+        guess_attempts += 1
+    else:
+        pass
+
+    print(f"Guess attempts = {guess_attempts}")
+        
+
+
+
+
+    # print(f"DEBUG: User guess = {user_guess}")
 
 
 
